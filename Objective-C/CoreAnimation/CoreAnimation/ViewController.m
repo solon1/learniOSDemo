36a67843db3a16f576bbd729174ad9e23f7426e2
solon1 <yyzorz@gmail.com>
2016-09-17 12:38:28 +0800

    add animation sample

--- Objective-C/CoreAnimation/CoreAnimation/ViewController.m
+++ Objective-C/CoreAnimation/CoreAnimation/ViewController.m
@@ -15,7 +15,8 @@
 @property (weak, nonatomic) IBOutlet UIView *viewThree;
 @property (weak, nonatomic) IBOutlet UIView *viewFour;
 
-
+@property (nonatomic,weak) UIView *secondHand;
+@property (nonatomic,strong) NSTimer *timer;
 @end
 
 @implementation ViewController
@@ -25,21 +26,81 @@
     [super viewDidLoad];
 //    [self setupLayerView];
 //    [self imageForView];
+    [self hideFourView];
     self.view.backgroundColor = [UIColor grayColor];
 //    [self stretchImageForView];
-    
-    
+//    [self drawWithDelegate];
+//    [self setClockView];
     
     // Do any additional setup after loading the view, typically from a nib.
 }
+
+#pragma mark - zPosition应用
+- (void)usezPositionInTwoViews
+{
+    UIView *viewOne = [UIView alloc] initWithFrame:CGRectMake(<#CGFloat x#>, <#CGFloat y#>, <#CGFloat width#>, <#CGFloat height#>)
+}
+
+#pragma mark - anchorPoint示例
+//通过设置钟表的指针解释anchorPoint的应用场景
+- (void)setClockView
+{
+    UIView *backView             = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 200, 200)];
+    backView.center              = self.view.center;
+    backView.backgroundColor     = [UIColor whiteColor];
+    backView.layer.cornerRadius  = 100.0;
+    backView.layer.masksToBounds = YES;
+    [self.view addSubview:backView];
+    //指针
+    UIView *secondHand             = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 2, 90)];
+    secondHand.backgroundColor = [UIColor redColor];
+    secondHand.center              = CGPointMake(100, 100);
+    [backView addSubview:secondHand];
+    self.secondHand = secondHand;
+    secondHand.layer.anchorPoint = CGPointMake(0.5, 0.9);//调整anchorPoint使秒钟围绕正确的中心点旋转
+    
+    self.timer = [NSTimer scheduledTimerWithTimeInterval:1.0 target:self selector:@selector(tick) userInfo:nil repeats:YES];
+    [self tick];
+}
+
+- (void)tick
+{
+    //转换时间到，时分秒
+    NSCalendar *calendar = [[NSCalendar alloc] initWithCalendarIdentifier:NSCalendarIdentifierGregorian];
+    NSInteger units = NSCalendarUnitHour | NSCalendarUnitMinute | NSCalendarUnitSecond;
+    NSDateComponents *components = [calendar components:units fromDate:[NSDate date]];
+    //计算时分秒的角度
+//    CGFloat hoursAngle = components.hour / 12.0f * M_PI * 2;
+//    CGFloat minuteAngle = components.minute / 60.f * M_PI * 2;
+    CGFloat secondAngle = components.second / 60.f * M_PI * 2;
+    
+    self.secondHand.transform = CGAffineTransformMakeRotation(secondAngle);
+}
+
 
 
 #pragma mark - CALayer代理绘制
 - (void)drawWithDelegate
 {
-    UIView *view = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 100, 100)];
+    UIView *view = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 200, 200)];
+    view.backgroundColor = [UIColor whiteColor];
     view.center = self.view.center;
+    [self.view addSubview:view];
     
+    CALayer *blueLayer = [CALayer layer];
+    blueLayer.frame = CGRectMake(50, 50, 100, 100);
+    blueLayer.backgroundColor = [UIColor blueColor].CGColor;
+    blueLayer.delegate = self;
+    blueLayer.contentsScale = [UIScreen mainScreen].scale;
+    [view.layer addSublayer:blueLayer];
+    [blueLayer display];
+}
+
+- (void)drawLayer:(CALayer *)layer inContext:(CGContextRef)ctx
+{
+    CGContextSetLineWidth(ctx, 10.0);
+    CGContextSetStrokeColorWithColor(ctx, [UIColor redColor].CGColor);
+    CGContextStrokeEllipseInRect(ctx, layer.bounds);
 }
 
 #pragma mark - contentsCenter图片拉升
@@ -77,6 +138,14 @@
     layer.contents = (__bridge id)image.CGImage;
     layer.contentsGravity = kCAGravityResizeAspect;
     layer.contentsRect = rect;
+}
+
+- (void)hideFourView
+{
+    self.layerView.hidden = YES;
+    self.viewTwo.hidden = YES;
+    self.viewThree.hidden = YES;
+    self.viewFour.hidden = YES;
 }
 
 #pragma mark - 第一节图层树与寄宿图
